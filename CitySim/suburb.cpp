08799abfe581695701c0a93179e3934b6e575a80
Patient Lion <103228157+patientlion@users.noreply.github.com>
2023-09-11 13:22:59 +1200

    added more comments

--- CitySim/suburb.cpp
+++ CitySim/suburb.cpp
@@ -3,17 +3,21 @@
 
 using namespace std;
 
+// Constructor to initialise the suburb with given ID
 Suburb::Suburb(int i) : id(i), migrated_in(0, 0, 0), migrated_out(0, 0, 0) {}
 
+// Function to determine the number of people preparing to move
 int Suburb::prepare_to_move() {
 	return rand() % (pop.total() + 1);
 }
 
+// Function to reset migration counters to initial state
 void Suburb::reset_migrated_counters() {
 	migrated_in = Population<int>();
 	migrated_out = Population<int>();
 }
 
+// Function to print current suburb's population details
 void Suburb::print() const {
 	cout << "\nSuburb " << id << ": Workers: " << pop.workers
 		<< ", Teachers: " << pop.teachers
@@ -21,6 +25,7 @@ void Suburb::print() const {
 		<< ", Total: " << pop.total() << "\n";
 }
 
+// Function to print migration statistics for the suburb
 void Suburb::print_migration_stats() const {
 	cout << "Suburb " << id << " Migration Stats: \n"
 		<< "Migrated In - Workers: " << migrated_in.workers
--- CitySim/suburb.h
+++ CitySim/suburb.h
@@ -11,14 +11,19 @@ public:
 	Population<int> migrated_out;    // Population migrated out of the suburb
 	std::vector<int> neighbour_ids;  // IDs of neighbouring suburbs
 
+	// Constructor to initialise the suburb with given ID
 	Suburb(int i);
 
+	// Function to determine the number of people preparing to move
 	int prepare_to_move();
 
+	// Function to reset migration counters to initial state
 	void reset_migrated_counters();
 
+	// Function to print current suburb's population details
 	void print() const;
 
+	// Function to print migration statistics for the suburb
 	void print_migration_stats() const;
 };
 #endif // SUBURB_H
